f568c8c960627ebe155ed5bc76735239050f65a9
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:33:25 +0300

    add const about

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+<p>heloo</p>
+    </div>
+  )
+}
+
+export default About
