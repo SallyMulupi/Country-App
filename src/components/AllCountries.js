e0b5bd8a003e48b22bdf96afdfe3dfe514795813
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:27:24 +0300

    add const all countries

--- src/components/AllCountries.js
+++ src/components/AllCountries.js
@@ -1,3 +1,7 @@
 import React,{useState} from 'react'
 import CountryInfo from './CountryInfo';
 import Search from "./Search";
+const AllCountries = ({continents}) => {
+    const [search, setSearch] = useState("");
+
+
