82ae93c9642bd61d0249a868fc5921d1243c5d25
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:32:01 +0300

    add const contact

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,1 +1,19 @@
 import React from 'react'
+const Contact = () => {
+    return (
+      <div>
+        <form>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        <input type="text" placeholder="Enter your name"/><br/>
+        </form>
+      </div>
+    )
+  }
+  
+  export default Contact
