65ac594a13888d2c0361d65bf906d500929c968e
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:35:34 +0300

    add function countryinfo

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,4 +1,16 @@
 import React from "react";
 
 function CountryInfo({ population,flags, capital, name }) {
-  return (
+  return (<>
+    <div className="foodcontainer">
+    <img className="foodimage" src={flags} alt="" />
+      <h4 className="">Country Name:-{name}</h4>
+      <p>Country capital:-{capital}</p>
+      <h4 className="">Country's population:-{population}</h4>
+    </div>
+  </>
+);
+//Mark as read
+}
+
+export default CountryInfo;
