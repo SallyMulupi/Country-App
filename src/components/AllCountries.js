538ff938e45aa4f5697b3f652e7a8a29918ee4fb
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:28:02 +0300

    add const countrytdisplay

--- src/components/AllCountries.js
+++ src/components/AllCountries.js
@@ -5,3 +5,13 @@ const AllCountries = ({continents}) => {
     const [search, setSearch] = useState("");
 
 
+    const countryTDisplay=continents.map((continent) => (
+        <CountryInfo
+        key={continent.id}
+        name={continent.name.common}
+        capital={continent.capital}
+        languages={continent.language}
+        flag={continent.flag}
+        flags={continent.flags.png}
+        population={continent.population}
+        />
