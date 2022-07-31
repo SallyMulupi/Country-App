9bba2b4ea85fcf9224b20ae2800f55e32b831657
SallyMulupi <mulupisally2gmail.com>
2022-07-31 22:23:26 +0300

    add imports

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const ListContainer = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const linkStyles = {
+  padding: "12px",
+  margin: "0 6px 6px",
+  textDecoration: "none",
+  color: "white",
+};
